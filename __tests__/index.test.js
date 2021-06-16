199fc5e7d38eddcfada6e9c7df083fab24dbf0e9
Boris P <pustovetov.boris@yandex.ru>
2021-06-17 02:23:20 +0300

    third refactoring from 13-06-2021

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,82 +9,80 @@ const inputURL = 'https://ru.hexlet.io/courses';
 const pageURL = new URL(inputURL);
 let tmpDir;
 nock.disableNetConnect();
-const scope = nock(pageURL.origin);
+let scope;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const filesDirectoryName = 'ru-hexlet-io-courses_files';
+const getFixturePath = (filename = '') => path.join(__dirname, '..', '__fixtures__', filename);
 
 const originalHtmlPath = getFixturePath('original_page.html');
-const expectHtmlPath = getFixturePath('ru-hexlet-io-courses.html');
-const expectCssPath = getFixturePath(path.join(filesDirectoryName, 'ru-hexlet-io-assets-application.css'));
-const expectPngPath = getFixturePath(path.join(filesDirectoryName, 'ru-hexlet-io-assets-professions-nodejs.png'));
-const expectSubHtmlPath = getFixturePath(path.join(filesDirectoryName, 'ru-hexlet-io-courses.html'));
-const expectScriptPath = getFixturePath(path.join(filesDirectoryName, 'ru-hexlet-io-packs-js-runtime.js'));
+const expectHtmlAbsolutePath = getFixturePath('ru-hexlet-io-courses.html');
+const expectCssRelativePath = path.join(filesDirectoryName, 'ru-hexlet-io-assets-application.css');
+const expectSubHtmlRelativePath = path.join(filesDirectoryName, 'ru-hexlet-io-courses.html');
+const expectPngRelativePath = path.join(filesDirectoryName, 'ru-hexlet-io-assets-professions-nodejs.png');
+const expectScriptRelativePath = path.join(filesDirectoryName, 'ru-hexlet-io-packs-js-runtime.js');
+
+const mapping = {
+  '/courses': originalHtmlPath,
+  '/assets/application.css': path.join(getFixturePath(), expectCssRelativePath),
+  '/assets/professions/nodejs.png': path.join(getFixturePath(), expectPngRelativePath),
+  '/packs/js/runtime.js': path.join(getFixturePath(), expectScriptRelativePath),
+};
+const pathNames = ['/courses', '/assets/application.css', '/assets/professions/nodejs.png', '/packs/js/runtime.js'];
 
 const readFile = async (filePath, encoding = 'utf-8') => {
-  const content = await fsp.readFile(filePath, encoding);
-  return content;
+  await fsp.readFile(filePath, encoding);
 };
 
 beforeEach(async () => {
   tmpDir = await fsp.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+  pathNames.forEach((pathName) => {
+    scope = nock(pageURL.origin)
+      .persist().get(pathName).replyWithFile(200, mapping[pathName]);
+  });
 });
 
 describe('Page-loader tests without errors expecting', () => {
   test('Is request working and get response with "OK" status', async () => {
-    scope.get(pageURL.pathname)
-      .reply(200, 'response');
     await pageLoader(inputURL, tmpDir);
 
     expect(scope.isDone()).toBe(true);
   });
-  test('Files were downloaded and html was modified', async () => {
-    const modifiedHtmlContent = await readFile(expectHtmlPath);
-    const expectedFilesTree = await fsp.readdir(getFixturePath(filesDirectoryName));
 
-    const mapping = [
-      ['/courses', originalHtmlPath],
-      ['/assets/application.css', expectCssPath],
-      ['/courses', expectSubHtmlPath],
-      ['/assets/professions/nodejs.png', expectPngPath],
-      ['/packs/js/runtime.js', expectScriptPath],
-    ];
-
-    mapping.forEach(([pagePath, filePath]) => scope.get(pagePath)
-      .replyWithFile(200, filePath));
+  test('Main html file was downloaded and modified', async () => {
+    const modifiedHtmlContent = await readFile(expectHtmlAbsolutePath);
 
     await pageLoader(inputURL, tmpDir);
 
     expect(await readFile(path.join(tmpDir, 'ru-hexlet-io-courses.html'))).toBe(modifiedHtmlContent);
-    expect(await fsp.readdir(path.join(tmpDir, 'ru-hexlet-io-courses_files'))).toEqual(expectedFilesTree);
+  });
 
-    const fixtureFiles = [
-      expectCssPath,
-      expectSubHtmlPath,
-      expectPngPath,
-      expectScriptPath,
-    ];
-    const savedFiles = fixtureFiles.map((filePath) => path.join(tmpDir, 'ru-hexlet-io-courses_files', path.parse(filePath).base));
+  test.each([
+    expectCssRelativePath,
+    expectSubHtmlRelativePath,
+    expectPngRelativePath,
+    expectScriptRelativePath,
+  ])('File from main page was downloaded and saved to %s', async (fixtureRelativeFilePath) => {
+    await pageLoader(inputURL, tmpDir);
 
-    savedFiles.forEach(async (savedFilePath, index) => {
-      const fixtureContent = await readFile(savedFilePath);
-      const savedContent = await readFile(fixtureFiles[index]);
-      expect(fixtureContent).toBe(savedContent);
-    });
+    const savedFilePath = path.join(tmpDir, fixtureRelativeFilePath);
+    const fixtureFilePath = path.join(getFixturePath(), fixtureRelativeFilePath);
+    const fixtureContent = await readFile(fixtureFilePath);
+    const savedContent = await readFile(savedFilePath);
+
+    expect(savedContent).toBe(fixtureContent);
   });
 });
 
 describe.each([
-  [404],
-  [504],
-  [502],
+  404,
+  504,
+  502,
 ])('Tests with network and server errors expecting', (error) => {
   test(`Get ${error} code error`, async () => {
-    scope.get(`/${error}`).reply();
-    await expect(pageLoader(inputURL, tmpDir)).rejects.toThrow(`Request error at ${inputURL}`);
+    await expect(pageLoader(`${pageURL.origin}/${error}`, tmpDir)).rejects.toThrow(`Request error at ${pageURL.origin}`);
   });
 });
 
@@ -95,13 +93,12 @@ describe('Tests with file system errors expecting', () => {
     [originalHtmlPath, 'is not a directory'],
   ])('Writing errors', (outputPath, errorText) => {
     test(`File errors contains "${errorText}"`, async () => {
-      scope.get(pageURL.pathname).reply(200, 'response');
       await expect(pageLoader(inputURL, outputPath)).rejects.toThrow(errorText);
     });
   });
   test('Output directory to webpage files already exists', async () => {
-    const expected = `File system error. ${path.join(tmpDir, 'ru-hexlet-io-courses_files')} already exists`;
-    scope.persist().get(pageURL.pathname).reply(200, 'response');
+    const existingPath = path.join(tmpDir, filesDirectoryName);
+    const expected = `File system error. ${existingPath} already exists`;
     await pageLoader(inputURL, tmpDir);
     await expect(pageLoader(inputURL, tmpDir)).rejects.toThrow(expected);
   });
