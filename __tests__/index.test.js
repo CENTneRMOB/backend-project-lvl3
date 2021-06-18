0c42b5c1860760ac3fd22ce64c999f345265f6ba
Boris P <pustovetov.boris@yandex.ru>
2021-06-18 15:30:26 +0300

    third refactoring from 13-06-2021

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,7 +9,7 @@ const inputURL = 'https://ru.hexlet.io/courses';
 const pageURL = new URL(inputURL);
 let tmpDir;
 nock.disableNetConnect();
-let scope;
+const scope = nock(pageURL.origin).persist();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -17,31 +17,31 @@ const __dirname = path.dirname(__filename);
 const filesDirectoryName = 'ru-hexlet-io-courses_files';
 const getFixturePath = (filename = '') => path.join(__dirname, '..', '__fixtures__', filename);
 
-const originalHtmlPath = getFixturePath('original_page.html');
 const expectHtmlAbsolutePath = getFixturePath('ru-hexlet-io-courses.html');
 const expectCssRelativePath = path.join(filesDirectoryName, 'ru-hexlet-io-assets-application.css');
 const expectSubHtmlRelativePath = path.join(filesDirectoryName, 'ru-hexlet-io-courses.html');
 const expectPngRelativePath = path.join(filesDirectoryName, 'ru-hexlet-io-assets-professions-nodejs.png');
 const expectScriptRelativePath = path.join(filesDirectoryName, 'ru-hexlet-io-packs-js-runtime.js');
 
-const mapping = {
-  '/courses': originalHtmlPath,
-  '/assets/application.css': path.join(getFixturePath(), expectCssRelativePath),
-  '/assets/professions/nodejs.png': path.join(getFixturePath(), expectPngRelativePath),
-  '/packs/js/runtime.js': path.join(getFixturePath(), expectScriptRelativePath),
-};
-const pathNames = ['/courses', '/assets/application.css', '/assets/professions/nodejs.png', '/packs/js/runtime.js'];
+const pathNamesWithFilePaths = [
+  ['/assets/application.css', expectCssRelativePath],
+  ['/courses', expectSubHtmlRelativePath],
+  ['/assets/professions/nodejs.png', expectPngRelativePath],
+  ['/packs/js/runtime.js', expectScriptRelativePath],
+];
 
 const readFile = async (filePath, encoding = 'utf-8') => {
-  await fsp.readFile(filePath, encoding);
+  const content = await fsp.readFile(filePath, encoding);
+  return content;
 };
+
+beforeAll(() => {
+  pathNamesWithFilePaths.forEach(([pathName, filePath]) => scope
+    .get(pathName).replyWithFile(200, path.join(getFixturePath(), filePath)));
+});
 
 beforeEach(async () => {
   tmpDir = await fsp.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
-  pathNames.forEach((pathName) => {
-    scope = nock(pageURL.origin)
-      .persist().get(pathName).replyWithFile(200, mapping[pathName]);
-  });
 });
 
 describe('Page-loader tests without errors expecting', () => {
@@ -59,12 +59,7 @@ describe('Page-loader tests without errors expecting', () => {
     expect(await readFile(path.join(tmpDir, 'ru-hexlet-io-courses.html'))).toBe(modifiedHtmlContent);
   });
 
-  test.each([
-    expectCssRelativePath,
-    expectSubHtmlRelativePath,
-    expectPngRelativePath,
-    expectScriptRelativePath,
-  ])('File from main page was downloaded and saved to %s', async (fixtureRelativeFilePath) => {
+  test.each(pathNamesWithFilePaths.map(([, filePath]) => filePath))('File from main page was downloaded and saved to %s', async (fixtureRelativeFilePath) => {
     await pageLoader(inputURL, tmpDir);
 
     const savedFilePath = path.join(tmpDir, fixtureRelativeFilePath);
@@ -90,7 +85,7 @@ describe('Tests with file system errors expecting', () => {
   describe.each([
     [(path.join('/var', 'lib')), 'no permissions to write in'],
     [(path.join('path', 'NotExists')), 'does not exist'],
-    [originalHtmlPath, 'is not a directory'],
+    [expectHtmlAbsolutePath, 'is not a directory'],
   ])('Writing errors', (outputPath, errorText) => {
     test(`File errors contains "${errorText}"`, async () => {
       await expect(pageLoader(inputURL, outputPath)).rejects.toThrow(errorText);
